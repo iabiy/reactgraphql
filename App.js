43cc6dcb62514c97c167ed768bbb4e54ae932e0b
iabiy <lijabiy@gmail.com>
2021-03-29 17:36:23 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App container mt-5">
+      <h1 className="text-primary"><i className="bi bi-diagram-2-fill"></i> iabiy
+      </h1>
+       
+    </div>
+  );
+}
+
+export default App;
